73275a5d032e8a6f9ef304aa2ac60ec72ee702f2
brain5775 <79450158+brain5775@users.noreply.github.com>
2023-05-20 18:09:21 +0700

    Rebuild

--- src/components/Particle.vue
+++ src/components/Particle.vue
@@ -3,16 +3,16 @@
 </template>
 
 <script setup>
-import { tsParticles } from "tsparticles-engine";
+// import { tsParticles } from "tsparticles-engine";
 
-tsParticles
-  .loadJSON("tsparticles", "presets/default.json")
-  .then((container) => {
-    console.log("callback - tsparticles config loaded");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// tsParticles
+//   .loadJSON("tsparticles", "presets/default.json")
+//   .then((container) => {
+//     console.log("callback - tsparticles config loaded");
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 </script>
 
 <style lang="scss" scoped></style>
